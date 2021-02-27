980f87d5d9d8d3c35e19e65ab5cf971e9a575b88
Brad Adams <hi@breadadams.com>
2021-02-27 04:52:27 +0100

    docs: update demo

--- demo/index.ts
+++ demo/index.ts
@@ -1,4 +1,5 @@
 import Prism from "prismjs";
+import "prismjs/components/prism-bash";
 import "prismjs/components/prism-json";
 import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-yaml";
